3be0e5ca301a48259184a464b5e4c61586c0dca7
Henna91 <noreply>
2015-04-30 07:25:22 +0000

    Add component home.html

---
+++ ui/home.html.reel/home.html.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/home.html.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Home.html
+ * @extends Component
+ */
+exports.Home.html = Component.specialize(/** @lends Home.html# */ {
+    constructor: {
+        value: function Home.html() {
+            this.super();
+        }
+    }
+});
