e47831c7fb2badd1c6e485570102d16e58d4dac6
Henna91 <noreply>
2015-04-30 07:23:57 +0000

    Add module home.html

---
+++ ui/home.html.js
@@ -0,0 +1,15 @@
+/**
+ * @module ui/home.html
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Home.html
+ * @extends Montage
+ */
+exports.Home.html = Montage.specialize(/** @lends Home.html# */ {
+    constructor: {
+        value: function Home.html() {
+            this.super();
+        }
+    }
+});
